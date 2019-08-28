768f164c88e3882189a35e9396f059a3c98e582e
AshutoshDKedar <ashutoshdkedar@gmail.com>
2019-08-28 12:06:26 +0530

    1 file modified, 1 added

---
+++ components/Registration.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+class Registration extends Component{
+  render(){
+    return(
+      <React.Fragment>
+        <h3>Create Your Account :-</h3>
+        <hr/>
+        <form className={classes.container} noValidate autoComplete="off">
+          <TextField id="standard-name" label="Name" margin="normal"/>
+        </form>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Registration;
--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import Button from '@material-ui/core/Button';
 import './style.css';
 
 import Title from './components/Title';
 import List from './components/List';
+import Registration from './components/Registration';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        
-        <List />
+        <Registration/>
       </React.Fragment>
     );
   }
